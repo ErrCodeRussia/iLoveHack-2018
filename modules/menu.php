1f43500240c23b379809a4281f971e7ccf68f9f5
CADExp <CADExp@ws.local>
2018-11-17 10:18:53 +0300

    menu template

---
+++ modules/menu.php
@@ -0,0 +1,35 @@
+<div class="flex-menu menu">
+    <nav class="ham">
+        <div id="menuToggle">
+            <label for="check"></label>
+            <input id="check" type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <li><a href="#top" class="ham-link">ГЛАВНАЯ</a></li>
+                <li><a href="#about" class="ham-link">ОБО МНЕ</a></li>
+                <li><a href="#services" class="ham-link">УСЛУГИ</a></li>
+                <li><a href="#portfolio" class="ham-link">ПОРТФОЛИО</a></li>
+                <li><a href="#help" class="ham-link">ПОДДЕРЖКА</a></li>
+                <li><a href="#contacts" class="ham-link">КОНТАКТЫ</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="nav-menu">
+        <div class="wrapper flex-menu">
+            <div class="logo font-roboto">
+                <div class="menu-nav"></div>
+                <a href="#top"><img src="img/art.png" alt="Логотип" class="logo-img"></a>
+            </div>
+            <nav class="navigation flex-menu font-roboto">
+                <a href="#top" class="nav-link active">ГЛАВНАЯ</a>
+                <a href="#about" class="nav-link">ОБО МНЕ</a>
+                <a href="#services" class="nav-link">УСЛУГИ</a>
+                <a href="#portfolio" class="nav-link">ПОРТФОЛИО</a>
+                <a href="#help" class="nav-link">ПОДДЕРЖКА</a>
+                <a href="#contacts" class="nav-link">КОНТАКТЫ</a>
+            </nav>
+        </div>
+    </div>
+</div>
