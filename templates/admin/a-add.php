8b629f22fbf2e44c8bcaa1897389495a800edd5b
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2018-11-17 17:37:46 +0300

    modified a-add.php [1]

--- templates/admin/a-add.php
+++ templates/admin/a-add.php
@@ -5,31 +5,31 @@
                 <legend class="mb-4">Добавить хакатон</legend>
                 <div class="form-group">
                     <label for="hack_name">Название хакатона</label>
-                    <input name="hack_name" type="text" class="form-control" id="hack_name" placeholder="iLovehack-2018">
+                    <input name="hack_name" type="text" class="form-control" id="hack_name" placeholder="iLovehack-2018" required>
                 </div>
                 <div class="form-group">
                     <label for="short_description">Краткое описание</label>
-                    <textarea class="form-control" name="hack_desc" id="short_description" rows="3" placeholder="Введите текст"></textarea>
+                    <textarea class="form-control" name="hack_desc" id="short_description" rows="3" placeholder="Введите текст" required></textarea>
                 </div>
                 <div class="form-group">
                     <label for="full_description">Полное описание</label>
-                    <textarea class="form-control" name="hack_full_desc" id="full_description" rows="3" placeholder="Введите текст"></textarea>
+                    <textarea class="form-control" name="hack_full_desc" id="full_description" rows="3" placeholder="Введите текст" required></textarea>
                 </div>
                 <div class="form-group">
                     <label for="hack_maxcount">Максимальное количество участников</label>
-                    <input name="hack_maxcount" type="text" class="form-control" id="hack_maxcount">
+                    <input name="hack_maxcount" type="text" class="form-control" id="hack_maxcount" required>
                 </div>
                 <div class="form-group">
                     <label for="hack_start_date">Дата начала хакатона</label>
-                    <input name="hack_start_date" type="date" class="form-control" id="hack_start_date">
+                    <input name="hack_start_date" type="date" class="form-control" id="hack_start_date" required>
                 </div>
                 <div class="form-group">
                     <label for="hack_end_date">Дата конца хакатона</label>
-                    <input name="hack_end_date" type="date" class="form-control" id="hachack_end_datek_name">
+                    <input name="hack_end_date" type="date" class="form-control" id="hachack_end_datek_name" required>
                 </div>
                 <div class="form-group">
                     <label for="status">Статус</label>
-                    <select name="hack_status" id="status" class="form-control">
+                    <select name="hack_status" id="status" class="form-control" required>
                         <option value="not-started">Не начался</option>
                         <option value="registration">Идёт регистрация</option>
                         <option value="conducts">Проводится</option>
