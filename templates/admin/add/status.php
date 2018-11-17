4512b205f20e8c72a63ed24fa85e745059cdb40a
Pavel Sklyar <ozpavel@yandex.ru>
2018-11-17 22:38:44 +0300

    Исправлен баг при создании таблицы конкретного хакатона - неправильно указывался id в новой таблице

--- templates/admin/add/status.php
+++ templates/admin/add/status.php
@@ -27,11 +27,6 @@ if (!empty($_POST)) {
     }
 
     $connection = get_connection();
-    
-    $get_last_string = mysqli_query($connection, "SELECT * FROM `hackathons` ORDER BY `hack_id` DESC LIMIT 1");
-    $last_string_array = mysqli_fetch_array($get_last_string);
-    $last_index = $last_string_array['hack_id'];
-    $last_index++;
 
     $create_table_status = mysqli_query($connection, "CREATE TABLE $hack_name 
     (
@@ -41,9 +36,13 @@ if (!empty($_POST)) {
     );");
     
     if ($create_table_status == true) {
-        $status_new_table = mysqli_query($connection, "INSERT INTO `$hack_name` (`hack_id`, `hack_teams`, `hack_scores`) VALUES ('$last_index', NULL, NULL);");
         $status = mysqli_query($connection, "INSERT INTO `hackathons` (`hack_id`, `hack_name`, `hack_desc`, `hack_full_desc`, `hack_count`, `hack_maxcount`, `hack_start_date`, `hack_end_date`, `hack_status`) VALUES (NULL, '$hack_name', '$hack_desc', '$hack_full_desc', '$hack_count', '$hack_maxcount', '$hack_start_date', '$hack_start_date', '$hack_status');");
     }
+
+    $get_last_string = mysqli_query($connection, "SELECT * FROM `hackathons` ORDER BY `hack_id` DESC LIMIT 1");
+    $last_string_array = mysqli_fetch_array($get_last_string);
+    $last_index = $last_string_array['hack_id'];
+    $status_new_table = mysqli_query($connection, "INSERT INTO `$hack_name` (`hack_id`, `hack_teams`, `hack_scores`) VALUES ('$last_index', NULL, NULL);");
 
     if ($status && $status_new_table) {
         echo "Хакатон успешно добавлен!";
