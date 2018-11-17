3a9c80dcf6a1184f7a37ee6ec256c4be70084169
Pavel Sklyar <ozpavel@yandex.ru>
2018-11-17 22:33:49 +0300

    Завершена работа над функционалом хакатонов [1]

--- templates/admin/a-delete.php
+++ templates/admin/a-delete.php
@@ -5,9 +5,15 @@
 <?php
 
     $connection = get_connection();
+    
+    $hackathon_info = mysqli_query($connection, "SELECT * FROM `hackathons` WHERE `hack_id` LIKE $params[0]");
+    $hackathon = mysqli_fetch_array($hackathon_info);
+    $hackathon_name = $hackathon['hack_name'];
+
+    $delete_table = mysqli_query($connection, "DROP TABLE `$hackathon_name`;");
     $delete = mysqli_query($connection, "DELETE FROM `hackathons` WHERE `hack_id` LIKE " . $params[0]);
 
-    if ($delete == true) {
+    if ($delete && $delete_table) {
         echo "Удаление прошло успешно.";
         header("Refresh: 1; url=/admin/");
     }
