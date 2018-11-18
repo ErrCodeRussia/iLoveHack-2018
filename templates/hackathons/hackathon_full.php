737f4135b360df92ef484b1b1739429c4879e668
Pavel <fateseekers@mail.ru>
2018-11-18 17:36:27 +0300

    final [1]

--- templates/hackathons/hackathon_full.php
+++ templates/hackathons/hackathon_full.php
@@ -26,7 +26,8 @@
         </div>
         <!-- IF BEGIN -->
         <div class="row">
-            <div class="col-12 d-flex justify-content-end">
+            <div class="col-12 d-flex flex-wrap justify-content-end">
+                <a href="/hackathones/" class="btn btn-dark full__hackathon__button ">Назад</a>
                 <input type="button" class="btn btn-dark full__hackathon__button" value="Регистрация">
                 <!-- Регистрация -->
                 <label for="file"><button class="btn btn-dark full__hackathon__button">Загрузить файлы</button></label>
