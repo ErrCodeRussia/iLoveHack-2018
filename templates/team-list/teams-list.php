de64408b862614e78eee3726d98289df96f5e325
Pavel <fateseekers@mail.ru>
2018-11-18 14:34:16 +0300

    Списки юзеров и команд

---
+++ templates/team-list/teams-list.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row mt-5">
+
+        <div class="col-12 table-responsive-sm overflow-auto">
+
+            <table class="table table-dark">
+                <caption class="table__heading">Список команд: </caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Id команды</th>
+                        <th scope="col">Название команды</th>
+                        <th scope="col">Количество участников</th>
+                        <th scope="col">Количество очков</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Выводишь циклом эту строку -->
+                    <tr>
+                        <th>Teamid</th>
+                        <td>TeamName</td>
+                        <td>TeamCount</td>
+                        <td>TeamScore</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
--- templates/users/u-list.php
+++ templates/users/u-list.php
@@ -1,1 +1,29 @@
-user list page
+<div class="container">
+    <div class="row mt-5">
+
+        <div class="col-12 table-responsive-sm overflow-auto">
+
+            <table class="table table-dark">
+                <caption class="table__heading">Список пользователей: </caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Логин пользователя</th>
+                        <th scope="col">Имя пользователя</th>
+                        <th scope="col">Фамилия пользователя</th>
+                        <th scope="col">Количество очков</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Выводишь циклом эту строку -->
+                    <tr>
+                        <th>UserLogin</th>
+                        <td>UserName</td>
+                        <td>UserSurname</td>
+                        <td>UserScore</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
