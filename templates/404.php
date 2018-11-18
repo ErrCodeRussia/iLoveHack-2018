fcd3b5a4a503b621ffa2c555c281b8cc1562f482
Pavel <fateseekers@mail.ru>
2018-11-18 16:21:20 +0300

    404

--- templates/404.php
+++ templates/404.php
@@ -1,1 +1,8 @@
-PAGE NOT FOUND
+<div class="container">
+    <div class="row error">
+        <div class="col-12 d-flex flex-column text-center">
+            <img class="error__img mx-auto" src="/img/close.svg" alt="">
+            <h2 class="error__heading">Страница не найдена :`(</h2>
+        </div>
+    </div>
+</div>
